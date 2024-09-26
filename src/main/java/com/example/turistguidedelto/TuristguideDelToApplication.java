df3bee27eda82a824796de041a76125e436bacaf
Salem <salo0002@stud.kea.dk>
2024-09-26 13:58:18 +0200

    Initial commit

---
+++ src/main/java/com/example/turistguidedelto/TuristguideDelToApplication.java
@@ -0,0 +1,13 @@
+package com.example.turistguidedelto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristguideDelToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristguideDelToApplication.class, args);
+	}
+
+}
